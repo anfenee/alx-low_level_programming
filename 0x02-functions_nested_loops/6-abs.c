bd951a4e412642bae1bcf18f93617645cc28b4fc
anfenee <mwangokeago@gmail.com>
2022-03-15 16:04:43 -0700

    anfenee

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - convert number to absolte
+ *@c: The int to convert to absolte
+ * Return: absolute value
+ */
+
+int _abs(int c)
+{
+if (c < 0)
+{
+int _absVal = c + (c * -2);
+return (absVal);
+}
+
+else
+return (c);
+
+}

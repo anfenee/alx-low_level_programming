7109049d5e863c578604f121f4fe3ceeb4badb1d
anfenee <mwangokeago@gmail.com>
2022-03-28 14:05:03 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -42,6 +42,5 @@ simple_print_buffer(buffer, 98);
 _memset(buffer, 0x01, 95);
 printf("-------------------------------------------------\n");
 simple_print_buffer(buffer, 98);    
-
-return (0);
+return (0)
 }
